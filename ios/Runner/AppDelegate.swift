a4024ae8988ad3dd1f278239620ffd987b82150b
kamize <talithanabila4c@gmail.com>
2023-06-06 18:51:37 +0700

    fixed API key for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,15 @@
 import UIKit
 import Flutter
-
+import GoogleMaps
+// Talitha Nabila - 1301204516
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  _ application: UIApplication,
+  didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey:
+  Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyC-PfYkXilmkIqftP3OymPfh2KWvAYgjpU")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,45 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+// Talitha Nabila - 1301204516
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
-
 // Talitha Nabila - 1301204516
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Talitha Nabila Networking Demo',
+      title: 'Talitha Nabila - 1301204516',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(title: 'Talitha Nabila - 1301204516'),
     );
   }
 }
-
-//Talitha Nabila - 1301204516
-class MyHomePage extends StatelessWidget {
+// Talitha Nabila - 1301204516
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+// Talitha Nabila - 1301204516
+class _MyHomePageState extends State<MyHomePage> {
+  static final LatLng _kMapCenter =
+      LatLng(19.018255973653343, 72.84793849278007);
+  static final CameraPosition _kInitialPosition =
+      CameraPosition(target: _kMapCenter, zoom: 11.0, tilt: 0, bearing: 0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Talitha Nabila Networking Demo'),
+        title: const Text('Talitha Nabila - 1301204516'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                deleteNotes('1');
-              },
-              child: Text('Delete Notes'),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                createNotes('New Notes');
-              },
-              child: Text('Create Notes'),
-            ),
-          ],
-        ),
+      body: GoogleMap(
+        initialCameraPosition: _kInitialPosition,
       ),
     );
-  }
-
-//Talitha Nabila - 1301204516
-  Future<void> deleteNotes(String id) async {
-    final http.Response response = await http.delete(
-      Uri.parse('https://jsonplaceholder.typicode.com/albums/$id'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-    );
-    if (response.statusCode == 200) {
-      print('Notes $id deleted successfully');
-    } else {
-      print('Failed to delete album. Error: ${response.statusCode}');
-    }
-  }
-
-//Talitha Nabila - 1301204516
-  Future<void> createNotes(String title) async {
-    final http.Response response = await http.post(
-      Uri.parse('https://jsonplaceholder.typicode.com/albums'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'title': title,
-      }),
-    );
-    if (response.statusCode == 201) {
-      print('Notes created successfully');
-      final Map<String, dynamic> album = jsonDecode(response.body);
-      print('New Notes ID: ${album['id']}');
-      print('New Notes Title: ${album['title']}');
-    } else {
-      print('Failed to create album. Error: ${response.statusCode}');
-    }
   }
 }
